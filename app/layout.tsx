a95cb7d697b45a429b08afdc90481f5e7758dc46
Avishek-works <136788543+Avishek-works@users.noreply.github.com>
2026-03-26 00:11:17 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,11 +17,11 @@ export const metadata: Metadata = {
 
   icons: {
     icon: [
-      { url: "/favicon.ico" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon_io/favicon.ico" },
+      { url: "/favicon_io/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon_io/favicon-16x16.png", sizes: "16x16", type: "image/png" },
     ],
-    apple: "/apple-touch-icon.png",
+    apple: "/favicon_io/apple-touch-icon.png",
   },
 }
 
