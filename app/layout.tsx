960a3884ddbb9ce2e379335a223d8fd4a20f5178
Avishek-works <136788543+Avishek-works@users.noreply.github.com>
2026-03-26 00:03:46 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,15 @@ export const metadata: Metadata = {
   title: 'EvolvNex',
   description: 'A high-end digital agency platform that transforms brands into scalable, automated growth systems.',
   generator: 'v0.app',
+
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
 }
 
 export default function RootLayout({
